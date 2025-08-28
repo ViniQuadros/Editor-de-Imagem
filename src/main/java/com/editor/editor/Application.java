7dc1bda2d92b7603911d7936bec5de376596b6c4
Lucas Michaelsen <lucasmicaoliveira@gmail.com>
2025-08-28 12:31:16 -0300

    Imagem na aba da janela

--- src/main/java/com/editor/editor/Application.java
+++ src/main/java/com/editor/editor/Application.java
@@ -2,7 +2,9 @@ package com.editor.editor;
 
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
+
 
 import java.io.IOException;
 
@@ -11,7 +13,8 @@ public class Application extends javafx.application.Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(Application.class.getResource("EditorDeImagens.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 900, 600);
-        stage.setTitle("Editor");
+        stage.setTitle("Visualise");
+        stage.getIcons().add(new Image(getClass().getResourceAsStream("/images/vizualize.png")));
         stage.setScene(scene);
         stage.show();
     }
